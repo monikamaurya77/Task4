8388caa00a555bfe73d446150f671e345528092a
Monika Maurya <96534851+monikamaurya77@users.noreply.github.com>
2023-03-01 22:59:52 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,9 +19,9 @@ const Search = () => {
         className="bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg"
       >
         <RiSearch2Line />
-      </button>
+      </button> 
     </div>
   );
 };
-
+//h
 export default Search;
